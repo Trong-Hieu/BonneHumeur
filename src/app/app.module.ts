636f64961483f6b6333ecaf5f73c581c7ce7b416
ngtrhieu0303 <ngtrhieu0303@gmail.com>
2020-09-08 10:11:37 +0700

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,17 @@ import { FirebaseServiceService } from './Services/firebase-service.service';
 import { TestComponent } from './test/test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NavBarComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +31,11 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     AngularFireDatabaseModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    RouterModule.forRoot([
+      { path: '', component: MainPageComponent }
+
+    ])
   ],
   providers: [
     FirebaseServiceService
