d613d304866c165c5c28fbaea5a3b3e43f6abd63
ngtrhieu0303 <ngtrhieu0303@gmail.com>
2020-12-07 16:20:21 +0700

    deploy with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA95hrE11QhXxz_KdWlOGrQsJgqVquvhjQ",
+    authDomain: "bonne-humuer.firebaseapp.com",
+    databaseURL: "https://bonne-humuer.firebaseio.com",
+    projectId: "bonne-humuer",
+    storageBucket: "bonne-humuer.appspot.com",
+    messagingSenderId: "466675731382",
+    appId: "1:466675731382:web:8c396e4f12cb15154bcf17",
+    measurementId: "G-HVLG4C5VV8"
+  }
 };
